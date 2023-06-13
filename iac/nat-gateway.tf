77058449040ec896ba18207c22dd6f20f77f7aee
Lucas Chagas <lucas.w.chagas@gmail.com>
2023-06-12 21:03:27 -0400

    updated depreciated code

--- iac/nat-gateway.tf
+++ iac/nat-gateway.tf
@@ -1,6 +1,6 @@
 # allocate elastic ip. this eip will be used for the nat-gateway in the public subnet az1 
 resource "aws_eip" "eip1" {
-  vpc = true
+  domain = "vpc"
 
   tags = {
     Name = "${var.project_name}-${var.environment}-eip1"
@@ -9,7 +9,7 @@ resource "aws_eip" "eip1" {
 
 # allocate elastic ip. this eip will be used for the nat-gateway in the public subnet az2
 resource "aws_eip" "eip2" {
-  vpc = true
+  domain = "vpc"
 
   tags = {
     Name = "${var.project_name}-${var.environment}-eip2"
